901336f15eb2739ad5ae71bb739034b3c1b01641
Matheus Lopes <matlopes1999@gmail.com>
2021-05-05 22:44:48 -0300

    add fonts, theme colors

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -5,26 +5,39 @@ class ProductItem extends StatelessWidget {
   final String title;
   final String imageUrl;
 
-  ProductItem(
-    this.id,
-    this.title,
-    this.imageUrl
-  );
+  ProductItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(imageUrl, fit: BoxFit.cover,),
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        leading: IconButton(
-          icon: Icon(Icons.favorite),
-          onPressed: () {},
-        ),
-        title: Text(title, textAlign: TextAlign.center,),
-        trailing: IconButton(
-          icon: Icon(Icons.shopping_cart),
-          onPressed: () {},
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: kElevationToShadow[2],
+        borderRadius: BorderRadius.circular(10)
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading: IconButton(
+              icon: Icon(Icons.favorite),
+              onPressed: () {},
+              color: Theme.of(context).accentColor,
+            ),
+            title: Text(
+              title,
+              textAlign: TextAlign.center,
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {},
+              color: Theme.of(context).accentColor,
+            ),
+          ),
         ),
       ),
     );
